f6a1d7777ff7b5b01df4434b879558f7138dbf49
pelden choda wangchuk <gokupelden@gmail.com>
2026-05-21 11:49:52 +0600

    added index.js todo.test.js dockerfile readme and pacakge.json

---
+++ Assignment3/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let todos = [];
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Todo App is running!', status: 'ok' });
+});
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const todo = {
+        id: todos.length + 1,
+        title: req.body.title,
+        done: false
+    };
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+
+app.listen(PORT, () => {
+    console.log(`Todo app running on port ${PORT}`);
+});
+
+module.exports = app;
